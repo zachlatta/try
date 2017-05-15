d0075ee418f9c02c9a2c7f541427854dfaf9e87c
Zach Latta <zach@zachlatta.com>
2017-05-15 07:46:10 -0700

    Only print env info if there is an env to be set

--- runner/shell/shell.go
+++ runner/shell/shell.go
@@ -25,16 +25,18 @@ func Start(dir string, env map[string]string) error {
 		Stdin:  os.Stdin,
 	}
 
-	fmt.Println("Starting", defaultShell+"...")
-	fmt.Println()
-	fmt.Println("> Please export the following environment variables:")
-	fmt.Println()
+	if len(env) > 0 {
+		fmt.Println("Starting", defaultShell+"...")
+		fmt.Println()
+		fmt.Println("> Please export the following environment variables:")
+		fmt.Println()
 
-	for _, env := range util.ConstructEnv(env, false) {
-		fmt.Println("export", env)
+		for _, env := range util.ConstructEnv(env, false) {
+			fmt.Println("export", env)
+		}
+
+		fmt.Println()
 	}
-
-	fmt.Println()
 
 	if err := cmd.Run(); err != nil {
 		return err
